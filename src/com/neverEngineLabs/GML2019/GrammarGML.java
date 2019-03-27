050c0f6c254dfb12ed4c8b50f4d4e7c72ddb49e4
cristianvogel <cristian.vogel55@gmail.com>
2019-03-27 06:05:52 -0700

    more Folder/file functionality added

--- src/com/neverEngineLabs/GML2019/GrammarGML.java
+++ src/com/neverEngineLabs/GML2019/GrammarGML.java
@@ -1,40 +1,115 @@
 package com.neverEngineLabs.GML2019;
+/*
+Generative Movement Grammar
 
+A token based score generator which uses context-free grammars
+and (optional) word lists to generate scores. By working with tokens
+as the mode of generated meaning, a score can be interpreted
+by human performers and/or virtual agents during realtime performance
+
+GML system and codebase was conceived and maintained by Cristian Vogel
+with essential contributions from Daniel Howe of RiTa lib and MDK of Korisna Media
+ */
+
+
+import java.awt.*;
 import java.io.File;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import com.sun.deploy.panel.ExceptionListDialog;
 import processing.core.PApplet;
 import rita.*;
 import rita.support.Conjugator;
+
+import static processing.core.PApplet.println;
+
 
 public class GrammarGML {
 
 	protected PApplet pApplet;
 	protected RiGrammar grammar;
 	protected Conjugator conjugator;
-	private String lineBreaker;
+	protected String pathToWordLists;
 
-	public GrammarGML(PApplet p) {this(p, "/");
+
+	private String lineBreaker;
+	private FileIOHelpers fileHelper;
+	private File wordListFolder;
+
+	public GrammarGML(PApplet p) {
+		this(p, "/");
 	}
-		
+
 	public GrammarGML(PApplet p, String lineBreakChar) {
 
-		this.pApplet = p;
-		this.lineBreaker = lineBreakChar;
-		this.conjugator = new Conjugator();
+		pApplet = p;
+		lineBreaker = lineBreakChar;
+		conjugator = new Conjugator();
+		fileHelper = new FileIOHelpers();
+
+		// default wordLists path todo: make user definable and disk stored preference?
+		pathToWordLists = "data/wordLists";
 	}
-	
+
 	public void loadFrom(String grammarFile) {
-		this.grammar = createGrammar(grammarFile);
+		println("Load Grammar from " + grammarFile);
+		// MDK : removed 'this.' from grammar, its redundant unless you
+		// need to resolve name collisions.
+		grammar = createGrammar(grammarFile);
+
+
+		//Now we've created the grammar, handle the word lists location
+		//default wordList path should be data/wordLists defined in pathToWorldLists
+
+		wordListFolder = new File(pathToWordLists);
+
+		//File dialog to handle lost default path for wordList data
+		//functionality will allow user to select folder location
+		//in future version
+
+		println("Word List Folder " + wordListFolder.getAbsolutePath());
+
+		println( "exists?" +  ((File) wordListFolder).exists());
+
+
+		fileHelper.checkFolderExistsOrGetUserLocation(wordListFolder, (File f) -> {
+			println("Folder selected " + f.getAbsolutePath());
+			// Here is where you can work with the selected file 'f'
+			// store it or whatever.
+			wordListFolder = f;
+			// Carry on...
+			loadWordLists();
+		});
+
 	}
-	
+
+	public RiGrammar createGrammar(String grammarFile) {
+
+		// MDK: If you only do this once I would move construction of RiGrammar into the GrammarGML constructor
+		RiGrammar rg = new RiGrammar();
+		rg.loadFrom(grammarFile, pApplet);
+
+		// MDK : Also we don't load the words immediately, we'll load them when the folder select has completed
+		return rg;
+	}
+
+	private void loadWordLists() {
+		String[] wordListFilenames = wordListFolder.list((dir, name) -> name.toLowerCase().endsWith(".txt"));
+
+		for (int i = 0; i < wordListFilenames.length; i++) {
+
+			String[] wordList = pApplet.loadStrings(wordListFolder.getAbsolutePath() + "/" + wordListFilenames[i]);
+			String ruleName = "<" + wordListFilenames[i].replaceAll(".txt", "") + ">";
+
+			String terminals = "";
+			for (int j = 0; j < wordList.length; j++) {
+				terminals += wordList[j] + " | ";
+			}
+			grammar.addRule(ruleName, terminals);
+		}
+	}
+
 	public String[] expand() {
-		
+
 		String[] lines = grammar.expand(this).split(lineBreaker);
 		for (int i = 0; i < lines.length; i++) {
 			lines[i] =  lines[i].trim();
@@ -42,55 +117,13 @@ public class GrammarGML {
 		return lines;
 	}
 
-	public RiGrammar createGrammar(String grammarFile) {
-		
-		RiGrammar rg = new RiGrammar();
-		rg.loadFrom(grammarFile, this.pApplet);
-		
-		String pathToWordLists = pApplet.dataPath("") + "/wordLists";
-
-		/*
-
-		would really rather have a check if this directory exists or not and
-		then open a file DirectoryBrowser so the user can find it
-		but I don't know how to do that
-
-		 */
-
-
-		File file = new java.io.File(pathToWordLists);
-
-
-		String[] wordListFilenames = file.list(movFilter);
-
-		for (int i = 0; i < wordListFilenames.length; i++) {
-			
-			String[] wordList = pApplet.loadStrings(pathToWordLists + "/" + wordListFilenames[i]);
-			String ruleName = "<" + wordListFilenames[i].replaceAll(".txt", "") + ">"; 
-			
-			String terminals = "";
-			for (int j = 0; j < wordList.length; j++) {
-				terminals += wordList[j] + " | ";
-			}
-			rg.addRule(ruleName, terminals);
-		}
-		
-		return rg;
-	}
-
-	java.io.FilenameFilter movFilter = new java.io.FilenameFilter() {
-		public boolean accept(File dir, String name) {
-			return name.toLowerCase().endsWith(".txt");
-		}
-	};
-	
 	// --------------------------- callbacks from grammars --------------------------------
 
 	String capitalise(String word) {
 
 		return RiTa.upperCaseFirst(word);
 	}
-	
+
 	String conjugate(String number, String person, String tense, String verb) {
 
 		if (tense.equals("ing")) {
@@ -106,16 +139,16 @@ public class GrammarGML {
 	}
 
 	String rand(float r) {
-		
+
 		return PApplet.str(PApplet.ceil(pApplet.random(1, r)));
 	}
 
 	String uniquePair(String ruleName, String prep) {
-		
+
 		String rule = '<' + ruleName + '>';
 		String a = grammar.expandFrom(rule), b = a;
 		while (a.equals(b)) b = grammar.expandFrom(rule);
 		return a + ' ' + prep + ' ' + b;
 	}
-	
+
 }
